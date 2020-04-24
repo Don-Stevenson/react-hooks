28789fca2c09efc6774c1e7ba4130e49bced9443
Don-Stevenson <dsteven1@gmail.com>
2020-04-23 22:02:16 -0400

    working count button with useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  console.log(useState())
-  return <div> Hey There</div>;
+  const [count, setCount] = useState(10);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}> + </button>
+      <div>{count}</div>
+    </div>
+  );
 };
 
 export default App;
