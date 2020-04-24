6b62b44bbc7667e7e37e561ebba59d1dae3f12f2
Don-Stevenson <dsteven1@gmail.com>
2020-04-23 22:10:48 -0400

    set up count1 and count2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(10);
+  const [{count1, count2}, setCount] = useState({count1: 10, count2:20});
 
   return (
     <div>
       <button onClick={() => setCount(currentcount => currentcount + 1)}> + </button>
-      <div>{count}</div>
+      <div>count 1: {count1}</div>
+      <div>count 2: {count2}</div>
     </div>
   );
 };
