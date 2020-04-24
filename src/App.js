4c29c3d94ee7f5cea10682045871656b8f23cb24
Don-Stevenson <dsteven1@gmail.com>
2020-04-24 14:04:48 -0400

    trying out mouse move logging to console

--- src/App.js
+++ src/App.js
@@ -2,25 +2,68 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "./useForm";
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "", firstName: "" });
+  // const [values, handleChange] = useForm({
+  //   email: "",
+  //   password: "",
+  //   firstName: ""
+  // });
 
   useEffect(() => {
-    console.log("Render");
-  }, [values.email]);
+    const onMouseMove = e => {
+      console.log(e);
+    };
+    window.addEventListener("mouse position", onMouseMove);
 
-  return (
-    <div>
-      <input name="firstName" placeholder="First name" values={values.firstName} onChange={handleChange} />
-      <input name="email" placeholder="email address" values={values.email} onChange={handleChange} />
-      <input
-        type="password"
-        name="password"
-        placeholder="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-    </div>
-  );
+    return () => {
+      window.removeEventListener("mouse position", onMouseMove);
+    };
+  }, []);
+
+  // return (
+  //   <div>
+  //     <input
+  //       name="firstName"
+  //       placeholder="First name"
+  //       values={values.firstName}
+  //       onChange={handleChange}
+  //     />
+  //     <input
+  //       name="email"
+  //       placeholder="email address"
+  //       values={values.email}
+  //       onChange={handleChange}
+  //     />
+  //     <input
+  //       type="password"
+  //       name="password"
+  //       placeholder="password"return (
+  //   <div>
+  //     <input
+  //       name="firstName"
+  //       placeholder="First name"
+  //       values={values.firstName}
+  //       onChange={handleChange}
+  //     />
+  //     <input
+  //       name="email"
+  //       placeholder="email address"
+  //       values={values.email}
+  //       onChange={handleChange}
+  //     />
+  //     <input
+  //       type="password"
+  //       name="password"
+  //       placeholder="password"
+  //       value={values.password}
+  //       onChange={handleChange}
+  //     />
+  //   </div>
+  // );
+  //       value={values.password}
+  //       onChange={handleChange}
+  //     />
+  //   </div>
+  // );
 };
 
 export default App;
