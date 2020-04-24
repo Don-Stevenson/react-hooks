ea4e96934889c1a77f10472fe64eb0113367230a
Don-Stevenson <dsteven1@gmail.com>
2020-04-24 13:46:32 -0400

    try useEffect and add placeholders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "./useForm";
 
 const App = () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
 
+  useEffect(() => {
+    console.log("Render");
+  }, [values.email]);
+
   return (
     <div>
-      <input name="email" values={values.email} onChange={handleChange}/>
+      <input name="email" placeholder="email address" values={values.email} onChange={handleChange} />
       <input
         type="password"
         name="password"
+        placeholder="password"
         value={values.password}
         onChange={handleChange}
       />
