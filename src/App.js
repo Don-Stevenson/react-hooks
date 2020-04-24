98936309e0bcc431c2d24a14fb1c0524ae4e3b9e
Don-Stevenson <dsteven1@gmail.com>
2020-04-23 22:21:10 -0400

    add second count and use state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [{count1, count2}, setCount] = useState({count1: 10, count2:20});
+  const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
 
   return (
     <div>
-      <button onClick={() => setCount(currentcount => currentcount + 1)}> + </button>
+      <button
+        onClick={() =>
+          setCount(currentState => ({
+            ...currentState,
+            count1: currentState.count1 + 1
+          }))
+        }
+      >
+        {" "}
+        +{" "}
+      </button>
       <div>count 1: {count1}</div>
       <div>count 2: {count2}</div>
     </div>
