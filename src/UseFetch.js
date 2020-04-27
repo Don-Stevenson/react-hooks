b387ecaaa987e35173001d6f147a91460220d8fa
Don-Stevenson <dsteven1@gmail.com>
2020-04-26 22:41:17 -0400

    update dependencies in useFetch

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 export const useFetch = url => {
-  const [state, setstate] = useState({ data: 0, loading: true });
+  const [state, setstate] = useState({ data: null, loading: true });
   
   useEffect(() => {
     setstate(state => ({ state: state.data, loading: true }));
     fetch(url)
       .then(x => x.text())
       .then(y => setstate({ data: y, loading: false }));
-  }, [url]);
+  }, [url, setstate]);
   return state;
 }; 
