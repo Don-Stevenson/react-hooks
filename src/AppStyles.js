bbc4b3451eae20a9ddefac1d8df6d7c2a4aa4c2e
Don-Stevenson <dsteven1@gmail.com>
2020-05-05 18:20:26 -0400

    update css

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -2,14 +2,18 @@ import styled from "styled-components";
 
 export const Div = styled.div`
   font-size: 20px;
-  margin: 20px;
+  margin: 9px;
   border: 15px outset lightblue;
   &:hover {
-    background-color: #A9E9E9;
+    background-color: #a9e9e9;
   }
 `;
 
 export const Paragraph = styled.p`
   font-size: 30px;
   text-align: center;
+  &:hover {
+    background-color: lightorange;
+    font-size: 40px;
+  }
 `;
